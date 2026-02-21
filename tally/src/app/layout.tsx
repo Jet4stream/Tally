2e2d14c8c96d64b71c6fc7458b41322c8481dfd6
Jet4stream <yotsuuye@gmail.com>
2026-02-21 11:18:34 -0500

    Setup Clerk

--- tally/src/app/layout.tsx
+++ tally/src/app/layout.tsx
@@ -1,19 +1,11 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { clerkMiddleware } from '@clerk/nextjs/server';
+// import { clerkMiddleware } from '@clerk/nextjs/server';
 import { ClerkProvider } from "@clerk/nextjs";
 
 // export default clerkMiddleware();
 
-export const config = {
-  matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
-    '/(api|trpc)(.*)',
-  ],
-};
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
